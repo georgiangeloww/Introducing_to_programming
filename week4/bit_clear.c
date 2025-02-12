c8e4731b31b8ba9c47322edb1f906827f258273f
georgiangeloww <georgigeorgievangelov5@gmail.com>
2025-02-12 23:44:55 +0200

    add bit_clear

---
+++ week4/bit_clear.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+int main(){
+    unsigned int n, r = 0;
+
+    printf("Enter number: ");
+    if (scanf("%u", &n) != 1){puts("Invalid input!"); return 1;}
+    printf("Enter bit you want to change: ");
+    if(scanf("%u", &r) != 1){puts("Invalid input!"); return 1;}
+
+
+
+    n = n & ~(1 << r);
+
+    printf("The new number: %u", n);
+
+    return 0; 
+}
